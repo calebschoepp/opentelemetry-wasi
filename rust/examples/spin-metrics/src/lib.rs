06b4e3378a35b38328a3645139efa13a4552857a
Andrew Steurer <94206073+asteurer@users.noreply.github.com>
2025-07-07 16:43:17 -0500

    saving work
    Signed-off-by: Andrew Steurer <94206073+asteurer@users.noreply.github.com>

--- rust/examples/spin-metrics/src/lib.rs
+++ rust/examples/spin-metrics/src/lib.rs
@@ -1,5 +1,9 @@
 use spin_sdk::http::{IntoResponse, Request, Response};
 use spin_sdk::http_component;
+use opentelemetry::metrics::{Counter, Histogram, Meter, MeterProvider};
+use opentelemetry::KeyValue;
+use opentelemetry_sdk::metrics::{SdkMeterProvider, PeriodicReader, MeterProviderBuilder};
+use opentelemetry_sdk::{resource, Resource};
 
 /// A simple Spin HTTP component.
 #[http_component]
@@ -10,4 +14,22 @@ fn handle_spin_metrics(req: Request) -> anyhow::Result<impl IntoResponse> {
         .header("content-type", "text/plain")
         .body("Hello World!")
         .build())
+}
+
+fn test() {
+    let meter_provider = SdkMeterProvider::builder()
+        .with_resource(Resource::new(vec![KeyValue::new("service.name", "my-service")]))
+        .build();
+
+    let meter = meter_provider.meter("my-meter");
+
+    let counter= meter
+        .u64_counter("http_requests_total")
+        .with_description("Total number of HTTP requests")
+        .with_unit("requests")
+        .build();
+
+    counter.add(15, &[KeyValue::new("status", "200")]);
+
+    
 }
