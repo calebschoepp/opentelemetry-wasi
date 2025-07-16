cda3094b78bed4b9b494e9b4f1ef3b7c0c08add0
Andrew Steurer <94206073+asteurer@users.noreply.github.com>
2025-07-15 23:54:07 -0500

    saving work
    Signed-off-by: Andrew Steurer <94206073+asteurer@users.noreply.github.com>

---
+++ rust/crates/metrics/src/lib.rs
@@ -0,0 +1,17 @@
+mod instrument;
+mod measurement;
+mod meter;
+
+#[doc(hidden)]
+/// Module containing wit bindgen generated code.
+///
+/// This is only meant for internal consumption.
+mod wit {
+    #![allow(missing_docs)]
+    #![allow(clippy::missing_safety_doc)]
+    wit_bindgen::generate!({
+        world: "wasi:otel/imports@0.2.0-draft",
+        path: "../../../wit",
+        generate_all,
+    });
+}
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,21 +1,5 @@
-mod conversion;
-mod processor;
-mod propagation;
+pub use opentelemetry_wasi_metrics as metrics;
+pub use opentelemetry_wasi_tracing as tracing;
 
-pub use processor::WasiProcessor;
-pub use propagation::TraceContextPropagator;
-pub use propagation::WasiPropagator;
-
-#[doc(hidden)]
-/// Module containing wit bindgen generated code.
-///
-/// This is only meant for internal consumption.
-mod wit {
-    #![allow(missing_docs)]
-    #![allow(clippy::missing_safety_doc)]
-    wit_bindgen::generate!({
-        world: "wasi:otel/imports@0.2.0-draft",
-        path: "../wit",
-        generate_all,
-    });
-}
+// pub use metrics::*;
+pub use tracing::*;
