241c31a62f6121c2fad732ce5efaaf09445d9b27
Andrew Steurer <94206073+asteurer@users.noreply.github.com>
2025-08-04 20:11:22 -0500

    saving work: switching from ydnar/wasi-http-go over to spin-sdk-go/http
    Signed-off-by: Andrew Steurer <94206073+asteurer@users.noreply.github.com>

--- go/wasip2/examples/spin-basic/go.mod
+++ go/wasip2/examples/spin-basic/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	go.bytecodealliance.org/cm v0.3.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/otel/metric v1.37.0 // indirect
@@ -14,8 +15,7 @@ require (
 
 require (
 	github.com/calebschoepp/opentelemetry-wasi v0.0.0
-	github.com/spinframework/spin-go-sdk/v2 v2.0.0-20250422162322-8ffe6d3efa29
-	github.com/ydnar/wasi-http-go v0.0.0-00010101000000-000000000000
+	github.com/spinframework/spin-go-sdk/v3 v3.0.0
 	go.opentelemetry.io/otel v1.37.0
 	go.opentelemetry.io/otel/sdk v1.37.0
 	go.opentelemetry.io/otel/trace v1.37.0
@@ -23,4 +23,4 @@ require (
 
 replace github.com/calebschoepp/opentelemetry-wasi => ../..
 
-replace github.com/ydnar/wasi-http-go => ../../../../../wasi-http-go
+replace github.com/spinframework/spin-go-sdk/v3 => ../../../../../spin-go-sdk/v3
--- go/wasip2/examples/spin-basic/main.go
+++ go/wasip2/examples/spin-basic/main.go
@@ -4,66 +4,59 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/ydnar/wasi-http-go/wasihttp"
+	spinhttp "github.com/spinframework/spin-go-sdk/v3/http"
 
 	otelWasi "github.com/calebschoepp/opentelemetry-wasi"
-	spinkv "github.com/spinframework/spin-go-sdk/v2/kv"
-	"github.com/spinframework/spin-go-sdk/v2/wit"
+	spinkv "github.com/spinframework/spin-go-sdk/v3/kv"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	sdkTrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/trace"
 )
 
-var _ = wit.Wit
-
 func init() {
-	wasihttp.Serve(&WasiHTTP{})
-}
+	spinhttp.Handle(func(w http.ResponseWriter, r *http.Request) {
+		wasiProcessor := otelWasi.NewWasiProcessor()
+		tracerProvider := sdkTrace.NewTracerProvider(
+			sdkTrace.WithSpanProcessor(wasiProcessor),
+		)
 
-type WasiHTTP struct{}
+		otel.SetTracerProvider(tracerProvider)
+		tracer := otel.Tracer("basic-spin")
 
-func (WasiHTTP) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	wasiProcessor := otelWasi.NewWasiProcessor()
-	tracerProvider := sdkTrace.NewTracerProvider(
-		sdkTrace.WithSpanProcessor(wasiProcessor),
-	)
+		wasiPropagator := otelWasi.NewTraceContextPropagator()
+		wasiPropagator.Extract(r.Context())
 
-	otel.SetTracerProvider(tracerProvider)
-	tracer := otel.Tracer("basic-spin")
+		ctx, span := tracer.Start(r.Context(), "main-operation")
+		defer span.End()
 
-	wasiPropagator := otelWasi.NewTraceContextPropagator()
-	wasiPropagator.Extract(r.Context())
+		span.SetAttributes(attribute.String("my-attribute", "my-value"))
+		span.AddEvent(
+			"Main span event",
+			trace.WithAttributes(attribute.String("foo", "1")),
+		)
 
-	ctx, span := tracer.Start(r.Context(), "main-operation")
-	defer span.End()
+		_, childSpan := tracer.Start(ctx, "child-operation")
+		childSpan.AddEvent(
+			"Sub span event",
+			trace.WithAttributes(attribute.String("bar", "1")),
+		)
+		defer childSpan.End()
 
-	span.SetAttributes(attribute.String("my-attribute", "my-value"))
-	span.AddEvent(
-		"Main span event",
-		trace.WithAttributes(attribute.String("foo", "1")),
-	)
+		store, err := spinkv.OpenDefault()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Failed to open kv store"))
+		}
 
-	_, childSpan := tracer.Start(ctx, "child-operation")
-	childSpan.AddEvent(
-		"Sub span event",
-		trace.WithAttributes(attribute.String("bar", "1")),
-	)
-	defer childSpan.End()
+		if err := store.Set("foo", []byte("bar")); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Failed to write to kv store"))
+		}
 
-	store, err := spinkv.OpenDefault()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Failed to open kv store"))
-	}
-
-	if err := store.Set("foo", []byte("bar")); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Failed to write to kv store"))
-	}
-
-	w.Header().Set("content-type", "text/plain")
-	fmt.Fprintln(w, "Hello, world!")
+		w.Header().Set("content-type", "text/plain")
+		fmt.Fprintln(w, "Hello, world!")
+	})
 }
 
 func main() {}
