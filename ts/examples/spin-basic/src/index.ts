e7d8c991064da81d63f312fea1df85b9e5279a16
Andrew Steurer <94206073+asteurer@users.noreply.github.com>
2025-12-04 16:26:51 -0600

    fix(tracing): kv store operations improperly nesting
    Signed-off-by: Andrew Steurer <94206073+asteurer@users.noreply.github.com>

--- ts/examples/spin-basic/src/index.ts
+++ ts/examples/spin-basic/src/index.ts
@@ -2,7 +2,7 @@ import { AutoRouter } from "itty-router";
 import { BasicTracerProvider } from "@opentelemetry/sdk-trace-base";
 import { WasiSpanProcessor, WasiTraceContextPropagator } from "opentelemetry-wasi";
 import { openDefault } from "@spinframework/spin-kv";
-import { context } from "@opentelemetry/api";
+import { context, trace } from "@opentelemetry/api";
 
 const propagator = new WasiTraceContextPropagator();
 const provider = new BasicTracerProvider({spanProcessors: [new WasiSpanProcessor()]});
@@ -11,22 +11,20 @@ provider.register();
 let router = AutoRouter();
 router
     .get("/", async () => {
-        let extractedContext = propagator.extract(context.active());
+        const hostContext = propagator.extract(context.active());
         let tracer = provider.getTracer("basic-spin");
-
-        return context.with(extractedContext, () => {
-            return tracer.startActiveSpan("main-operation", {}, extractedContext, (parentSpan) => {
-                parentSpan.setAttribute("my-attribute", "my-value");
-                parentSpan.addEvent("Main span event", {"foo": "1"} );
-                tracer.startActiveSpan("child-operation", {}, extractedContext, (childSpan) => {
-                    childSpan.addEvent("Sub span event", {"bar": "1"});
-                    let store = openDefault();
-                    store.set("foo", "bar");
-                    childSpan.end();
-                });
-                parentSpan.end();
-                return new Response("Hello, world!");
+        return tracer.startActiveSpan("main-operation", {}, hostContext, (parentSpan) => {
+            const parentContext = trace.setSpan(hostContext, parentSpan);
+            parentSpan.setAttribute("my-attribute", "my-value");
+            parentSpan.addEvent("Main span event", {"foo": "1"} );
+            tracer.startActiveSpan("child-operation", {}, parentContext, (childSpan) => {
+                childSpan.addEvent("Sub span event", {"bar": "1"});
+                let store = openDefault();
+                store.set("foo", "bar");
+                childSpan.end();
             });
+            parentSpan.end();
+            return new Response("Hello, world!");
         });
     });
 
